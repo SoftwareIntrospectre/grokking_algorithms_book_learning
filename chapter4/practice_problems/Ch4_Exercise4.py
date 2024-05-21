76d16265820eaf3777b258806ac470f191ffd46f
Software Introspectre <softwareintrospectre@gmail.com>
2024-05-21 10:16:17 -0400

    Recursive Binary Search function exercise completed

--- chapter4/practice_problems/Ch4_Exercise4.py
+++ chapter4/practice_problems/Ch4_Exercise4.py
@@ -8,24 +8,48 @@
 # base case: when an element is found that's searched. So if guess == an element in the array.
     # example: search for 7. 7 is found. Return 7.   OR Array is empty, return None.
 
-def recursive_binary_search(arr, search_item):
+def recursive_binary_search(arr, item, low=0, high=None):
 
-    found = None
+    # this only needs to be sorted once. No need to do this recursively
+    #arr.sort()
+    #print(arr)
 
-    print("arr: ", arr)
-    #print("search_item", search_item)
+    # initial value handling
+    if high is None:
+        high = len(arr) - 1
 
-    if len(arr) == 1:
-        if search_item == arr[0]:
-            found = search_item
-            return found
+    if low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+  
+        # base case 1: item is found, return
+        if guess == item:
+            print(guess)
+            return guess
         
-    elif len(arr) > 1:
-        arr.pop()
-        return recursive_binary_search(arr[1:], search_item)
-    
-    else:
-        return None
-    
-recursive_binary_search([1,8,9,10,12], 99)
-recursive_binary_search([1,8,9,10,12], 8)
+        # recursive case 1: guess is too high. Search up to to new high point (mid)
+        elif guess > item:
+
+            # defining new value for high, instead of storing as a variable
+            return recursive_binary_search(arr, item, low, mid - 1)
+
+        # recursive case 1: guess is too low. Search from new low point (mid + 1)
+        else:
+            # defining new value for low, instead of storing as a variable
+            return recursive_binary_search(arr, item, mid + 1, high)
+
+    # base case 2: No item is found, return None
+    print(None)
+    return None
+
+
+array1 = sorted([1,8,9,10,12,99])
+array2 = sorted([8,9,99,12])
+array3 = sorted([8,9,99,12,1478,6])
+array4 = sorted([8,9,99,12,1478,6])
+
+
+recursive_binary_search(array1, 99)
+recursive_binary_search(array2, 8)
+recursive_binary_search(array3, 12)
+recursive_binary_search(array4, 0)
